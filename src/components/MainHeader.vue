d21bf5040a0bb7076bc69763a63e91ea332768aa
Ariana <ajadrejci@unipu.hr>
2024-02-25 19:53:05 +0100

    add header component

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <p class="title">
+            {{headerText}} 
+        </p>
+    </header>
+</template>
+
+
+<script>
+
+export default {
+  name: 'MainHeader',
+  props: {
+    headerText: {
+        type: String,
+        required: true
+    }
+  },
+}
+
+</script>
+
+
+<style scoped>
+
+header {
+  background-color: #FFC2D1;
+  height: 100px;
+}
+
+.title {
+    font-size: 36px;
+    color: #FB6F92;
+}
+
+
+
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,14 @@
 <template>
-    login view
+    <main-header :headerText="'Login'"></main-header>
 </template>
 
 <script>
+import MainHeader from '@/components/MainHeader.vue'
 
 export default {
   name: 'LoginView',
   components: {
-    
+    MainHeader
   },
 }
 
