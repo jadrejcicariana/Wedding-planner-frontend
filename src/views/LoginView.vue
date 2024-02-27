eafbd6844de29a221bf02bdadb52745400bf2b58
Ariana <ajadrejci@unipu.hr>
2024-02-27 11:34:18 +0100

    edit login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,19 @@
 <template>
+  <header>
     <main-header :headerText="'Login'"></main-header>
+  </header>
+  <body>
+    <div class= "login-background">
+      <div class= "text">
+        <p class= "text1">
+          Don't have an account?
+        </p>
+        <p class = "text2" @click = "$router.push('Signup')">
+          Sign up
+        </p>
+      </div>
+    </div>
+  </body>
 </template>
 
 <script>
@@ -15,5 +29,24 @@ export default {
 </script>
 
 <style scoped>
+
+.login-background {
+  position: absolute;
+  top: 300;
+  left: 50;
+  width: 100%;
+  height: 100%;
+  background-color: #FFE5EC;
+  background-size: cover;
+  background-position: center;
+}
+
+.text1, .text2 {
+  display: inline;
+}
+
+.text2 {
+  color: #FB6F92;
+}
 
 </style>
