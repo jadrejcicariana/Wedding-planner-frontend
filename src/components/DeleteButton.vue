abb1bb8d21be13e44c3bf1e54fbc19ff23d7143f
Ariana <ajadrejci@unipu.hr>
2024-03-05 10:17:41 +0100

    add DeleteButton

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button @click = "onClick">
+        Delete
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'DeleteButton',
+  methods: {
+    onClick() {
+        this.$emit('click');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+Button {
+    position: relative;
+    width: 64px;
+    height: 40px;
+    color: black;
+    background-color: #ff0000;
+    margin: 10px;
+    border-radius: 16px;
+}
+
+</style>
