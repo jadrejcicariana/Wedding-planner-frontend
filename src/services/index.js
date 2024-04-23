bdda006f40816bfa815781e6d92b4765fde2ea80
Ariana <ajadrejci@unipu.hr>
2024-04-23 20:48:23 +0200

    update axios base url

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let Service = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://wedding-planner-backend-1.onrender.com',
     timeout: 1000
 })
 
